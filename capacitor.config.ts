6d7fa5fd4efd511000b5bf973963c6c6ccdddf6f
macortesro <147458427+macortesro@users.noreply.github.com>
2024-01-22 21:51:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'examen.ph.mcr',
+  appName: 'examen-ph-mcr',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
